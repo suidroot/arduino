79fc7fe3699979cc32f252e9929e17c7013a145b
Ben Mason <su1droot@gmail.com>
2017-07-22 17:50:55 -0400

    Updated Luminence Code, other misc optimization

--- TempHumLumSensor.ino
+++ TempHumLumSensor.ino
@@ -1,13 +1,14 @@
 /*
  * Tempurature Humidity and Luminance Sensor outputing to a SPI Screen
- * 
+ *
  * Components
  * Adafruit Feather HuzzaH
  * DHT22
+ * 27k Resistor
  * Photo Resistor
  * 10k Resistor
  * SSD1306 OLED Display
- * 
+ *
  */
 
 
@@ -35,8 +36,8 @@ Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 #define YPOS 1
 #define DELTAY 2
 
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
 
 // DHT Parameters (Tempurature Humitiy Sensor)
 #define DHTPIN            2         // Pin which is connected to the DHT sensor.
@@ -44,11 +45,16 @@ Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
-// Define Analogue Pin for Light Mesurement
+// Define Analogue Pin for Light Mesurement ESP-12 Max 1v
 #define LIGHTPIN A0
+#define MINLIGHT 500
+#define MAXLIGHT 900
+
+// Set rate of refresh in ms
+#define REFRESHDELAY 1500
 
 void setup() {
-  Serial.begin(9600); 
+  //Serial.begin(9600);
   // Initialize DHT device.
   dht.begin();
   sensor_t sensor;
@@ -56,19 +62,19 @@ void setup() {
   // Intialize the Screen
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC);
-  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
 }
 
 
 void loop() {
 
   // Setup Display
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
+  display.clearDisplay();
   display.setCursor(0,0);
 
   // Get temperature event and print its value.
-  sensors_event_t event;  
+  sensors_event_t event;
   dht.temperature().getEvent(&event);
 
   if (isnan(event.temperature)) {
@@ -91,13 +97,13 @@ void loop() {
   }
 
   // Get Light Reading from Analogue Pin
+  // ESP-12 Max 1v
   display.print("Lum: ");
-  display.print(map(analogRead(LIGHTPIN), 0, 1024, 0, 100));
+  //display.print(analogRead(LIGHTPIN));
+  display.print(map(analogRead(LIGHTPIN), MINLIGHT, MAXLIGHT, 0, 100));
   display.println("%");
 
   display.display();
-  delay(1500);
-  display.clearDisplay();
-
+  delay(REFRESHDELAY);
 
 }
